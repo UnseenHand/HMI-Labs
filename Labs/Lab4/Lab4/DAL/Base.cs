abc72ec86f3560b4ed29aeeb86f55628fcec8e0b
Vitaliy Ugera <shnobel12@gmail.com>
2022-11-22 15:33:40 +0200

    Added Lab2(Closed) and Lab4(In Progress)

---
+++ Labs/Lab4/Lab4/DAL/Base.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4.DAL
+{
+    internal class Base
+    {
+        public int Id { get; set; }
+        [Range()]
+        public string Description { get; set; }
+        
+    }
+}
